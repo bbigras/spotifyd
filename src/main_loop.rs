ddb92c772b22d01e097682418ac301444c0cb34b
Bruno Bigras <bigras.bruno@gmail.com>
2020-04-19 02:08:37 -0400

    WIP: support PropertiesChanged event

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -23,7 +23,7 @@ use tokio_io::IoStream;
 use std::{io, rc::Rc};
 
 #[cfg(feature = "dbus_mpris")]
-use crate::dbus_mpris::DbusServer;
+use crate::dbus_mpris::{ChangedTrack, DbusServer, TrackChange};
 use crate::process::{spawn_program_on_event, Child};
 
 pub struct LibreSpotConnection {
@@ -69,12 +69,14 @@ fn new_dbus_server(
     handle: Handle,
     spirc: Rc<Spirc>,
     device_name: String,
+    rx: futures::sync::mpsc::UnboundedReceiver<TrackChange>,
 ) -> Option<Box<dyn Future<Item = (), Error = ()>>> {
     Some(Box::new(DbusServer::new(
         session,
         handle,
         spirc,
         device_name,
+        rx,
     )))
 }
 
@@ -100,6 +102,11 @@ pub(crate) struct MainLoopState {
     pub(crate) running_event_program: Option<Child>,
     pub(crate) shell: String,
     pub(crate) device_type: DeviceType,
+    pub(crate) session: Option<Session>,
+    #[cfg(feature = "dbus_mpris")]
+    pub(crate) future_get_metadata: Option<Box<dyn Future<Item = (), Error = ()>>>,
+    #[cfg(feature = "dbus_mpris")]
+    pub(crate) tx: Option<futures::sync::mpsc::UnboundedSender<TrackChange>>,
 }
 
 impl Future for MainLoopState {
@@ -108,6 +115,16 @@ impl Future for MainLoopState {
 
     fn poll(&mut self) -> Poll<(), ()> {
         loop {
+            #[cfg(feature = "dbus_mpris")]
+            {
+                if let Some(future_get_metadata2) = &mut self.future_get_metadata {
+                    if let Async::Ready(()) = future_get_metadata2.poll().unwrap() {
+                        println!("ready");
+                        self.future_get_metadata = None;
+                    }
+                }
+            }
+
             if let Async::Ready(Some(creds)) =
                 self.librespot_connection.discovery_stream.poll().unwrap()
             {
@@ -136,9 +153,91 @@ impl Future for MainLoopState {
                 {
                     if let Async::Ready(Some(event)) = player_event_channel.poll().unwrap() {
                         if let Some(ref cmd) = self.spotifyd_state.player_event_program {
-                            match spawn_program_on_event(&self.shell, cmd, event) {
+                            match spawn_program_on_event(&self.shell, cmd, event.clone()) {
                                 Ok(child) => self.running_event_program = Some(child),
                                 Err(e) => error!("{}", e),
+                            }
+                        }
+
+                        #[cfg(feature = "dbus_mpris")]
+                        {
+                            use librespot::metadata::{Album, Artist, Metadata, Track};
+                            match event {
+                                PlayerEvent::Changed { new_track_id, .. } => {
+                                    let session_clone1 = self.session.as_ref().unwrap();
+                                    let session_clone2 = self.session.as_ref().unwrap().clone();
+                                    let session_clone3 = self.session.as_ref().unwrap().clone();
+                                    let tx2 = self.tx.as_ref().unwrap().clone();
+
+                                    let f = Track::get(&session_clone1, new_track_id)
+                                        .and_then(move |plist_track| {
+                                            let artist_id = *plist_track.artists.get(0).unwrap();
+
+                                            Album::get(&session_clone2, plist_track.album)
+                                                .and_then(move |r| {
+                                                    Artist::get(&session_clone3, artist_id)
+                                                        .map(|artist| (artist, r))
+                                                })
+                                                .map(|(artist, r)| (r, plist_track, artist))
+                                        })
+                                        .and_then(move |(album, track, artist)| {
+                                            println!("song: {}, album: {}", track.name, album.name);
+
+                                            tx2.unbounded_send(TrackChange::Changed(
+                                                ChangedTrack {
+                                                    track: track.name,
+                                                    album: album.name,
+                                                    artist: artist.name,
+                                                },
+                                            ))
+                                            .unwrap();
+                                            Ok(())
+                                        })
+                                        .map_err(|e| {
+                                            println!("some err: {:?}", e);
+                                        });
+                                    self.future_get_metadata = Some(Box::new(f));
+                                    continue;
+                                }
+                                PlayerEvent::Started { track_id } => {
+                                    println!("PlayerEvent::started zz");
+
+                                    let session = &self.session.as_ref().unwrap();
+                                    let tx2 = self.tx.as_ref().unwrap().clone();
+
+                                    let f = Track::get(&session, track_id)
+                                        .and_then(move |plist_track| {
+                                            println!("track: {} ", plist_track.name);
+
+                                            tx2.unbounded_send(TrackChange::Started).unwrap();
+
+                                            Ok(())
+                                        })
+                                        .map_err(|e| {
+                                            println!("some err: {:?}", e);
+                                        });
+                                    self.future_get_metadata = Some(Box::new(f));
+                                    continue;
+                                }
+                                PlayerEvent::Stopped { track_id } => {
+                                    println!("PlayerEvent::stopped zz");
+
+                                    let session = &self.session.as_ref().unwrap();
+                                    let tx2 = self.tx.as_ref().unwrap().clone();
+
+                                    let f = Track::get(&session, track_id)
+                                        .and_then(move |track| {
+                                            println!("track: {} ", track.name);
+                                            tx2.unbounded_send(TrackChange::Stopped).unwrap();
+
+                                            Ok(())
+                                        })
+                                        .map_err(|e| {
+                                            println!("some err: {:?}", e);
+                                        });
+                                    self.future_get_metadata = Some(Box::new(f));
+                                    continue;
+                                }
                             }
                         }
                     }
@@ -150,6 +249,7 @@ impl Future for MainLoopState {
             }
 
             if let Async::Ready(session) = self.librespot_connection.connection.poll().unwrap() {
+                self.session = Some(session.clone());
                 let mixer = (self.audio_setup.mixer)();
                 let audio_filter = mixer.get_audio_filter();
                 self.librespot_connection.connection = Box::new(futures::future::empty());
@@ -180,12 +280,29 @@ impl Future for MainLoopState {
                 let shared_spirc = Rc::new(spirc);
                 self.librespot_connection.spirc = Some(shared_spirc.clone());
 
-                self.spotifyd_state.dbus_mpris_server = new_dbus_server(
-                    session,
-                    self.handle.clone(),
-                    shared_spirc,
-                    self.spotifyd_state.device_name.clone(),
-                );
+                #[cfg(feature = "dbus_mpris")]
+                {
+                    let (tx, rx) = futures::sync::mpsc::unbounded::<TrackChange>();
+                    self.tx = Some(tx);
+
+                    self.spotifyd_state.dbus_mpris_server = new_dbus_server(
+                        session,
+                        self.handle.clone(),
+                        shared_spirc,
+                        self.spotifyd_state.device_name.clone(),
+                        rx,
+                    );
+                }
+
+                #[cfg(not(feature = "dbus_mpris"))]
+                {
+                    self.spotifyd_state.dbus_mpris_server = new_dbus_server(
+                        session,
+                        self.handle.clone(),
+                        shared_spirc,
+                        self.spotifyd_state.device_name.clone(),
+                    );
+                }
             } else if let Async::Ready(_) = self.spotifyd_state.ctrl_c_stream.poll().unwrap() {
                 if !self.spotifyd_state.shutting_down {
                     if let Some(ref spirc) = self.librespot_connection.spirc {
